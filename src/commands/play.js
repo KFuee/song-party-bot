a5bf85b9d8bc5ee9a93ebb7a051113f2bfd71613
KFuee <jbisazac@icloud.com>
2021-12-16 12:59:27 +0100

    Obtener canciones aleatorias y reproducción de canción especifica

--- src/commands/play.js
+++ src/commands/play.js
@@ -26,35 +26,29 @@ module.exports = {
       return;
     }
 
-    // Obtiene todos los miembros del canal de voz
-    const players = voiceChannel.members.map((player) => player.id);
-
-    // Crea una instancia de la clase Game
-    const game = new Game(players);
-
-    // Añade la partida a la colección de partidas
-    client.games.set(game.id, game);
-
     // Instancia un nuevo objeto SongPartyMusic
     const music = new SongPartyMusic(guild);
 
     // Conecta el cliente al canal de voz
     music.connect(textChannel, voiceChannel.id);
 
-    // Busca la canción introducida por el usuario
-    const res = await client.manager.search("bad bunny", userId);
+    // Obtiene 5 canciones aleatorias de la playlist
+    const songs = await music.getRandomSongs(
+      5,
+      "https://www.youtube.com/watch?v=J4_W-R3iPJ8&list=PLxZHtuv5hUL94eMtcOOV0BiZu6a4cRo4J"
+    );
 
-    // Obtiene el reproductor
-    const player = client.manager.players.get(guildId);
+    // Obtiene todos los miembros del canal de voz
+    const players = voiceChannel.members.map((player) => player.id);
 
-    // Añade la canción al reproductor
-    player.queue.add(res.tracks[0]);
+    // Crea una instancia de la clase Game
+    const game = new Game(players, songs);
 
-    // Comprueba si el reproductor está reproduciendo
-    if (!player.playing && !player.paused && !player.queue.size) {
-      // Reproduce la canción
-      player.play();
-    }
+    // Añade la partida a la colección de partidas
+    client.games.set(game.id, game);
+
+    // Reproduce la primera canción
+    music.play(game.playlist[0]);
 
     // Envía el mensaje de confirmación
     return interaction.reply({ content: "¡Jugando!" });
--- src/structures/SongPartyMusic.js
+++ src/structures/SongPartyMusic.js
@@ -13,6 +13,19 @@ class SongPartyMusic {
     return this.client.manager.players.get(this.guild.id) || null;
   }
 
+  // Obtiene n canciones aleatorias de la playlist
+  async getRandomSongs(n, playlist) {
+    // Obtiene la playlist
+    const res = await this.player.search(playlist, this.guild.id);
+
+    // Obtiene las canciones aleatorias
+    const songs = res.tracks.slice(0, n);
+
+    // Devuelve las canciones
+    return songs;
+  }
+
+  // Se conecta al canal de voz
   connect(text, voice) {
     // Comprueba si existe un reproductor en el servidor
     if (this.player) {
@@ -30,6 +43,27 @@ class SongPartyMusic {
     // Comprueba si ya se ha conectado a un canal de voz
     if (player.state !== "CONNECTED") {
       player.connect();
+    }
+  }
+
+  // Reproduce una canción
+  play(song) {
+    // Comprueba si existe un reproductor en el servidor
+    if (!this.player) {
+      return;
+    }
+
+    // Añade la canción al reproductor
+    this.player.queue.add(song);
+
+    // Comprueba si el reproductor está reproduciendo
+    if (
+      !this.player.playing &&
+      !this.player.paused &&
+      !this.player.queue.size
+    ) {
+      // Reproduce la canción
+      this.player.play();
     }
   }
 }
